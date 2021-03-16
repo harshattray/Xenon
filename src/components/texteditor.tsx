50c1d37ab9e0a157cb66dda4687b1a66d3acd491
harshattray <harsh.attray@gmail.com>
2021-03-16 14:41:24 +0100

    Markdown Editor fixes and bunch of Css added

--- src/components/texteditor.tsx
+++ src/components/texteditor.tsx
@@ -1,38 +1,48 @@
-import MDEditor from '@uiw/react-md-editor';
-import { useState, useEffect, useRef } from 'react';
-
+import MDEditor from "@uiw/react-md-editor";
+import "./texteditor.css";
+import { useState, useEffect, useRef } from "react";
 
 const MarkDownEditor: React.FC = () => {
-    //state used to switch between 
-    const [editing, setEditing] = useState(false)
+    //state used to switch between
+    const [editing, setEditing] = useState(false);
+    const [markDownValue, setMarkDownValue] = useState("# Header");
     const watcherRef = useRef<HTMLDivElement | null>(null);
 
     useEffect(() => {
         const listener = (event: MouseEvent) => {
-            if (watcherRef.current && event.target && watcherRef.current.contains(event.target as Node)) {
+            if (
+                watcherRef.current &&
+                event.target &&
+                watcherRef.current.contains(event.target as Node)
+            ) {
                 return;
             }
-            setEditing(false)
-        }
-        document.addEventListener('click', listener, { capture: true })
+            setEditing(false);
+        };
+        document.addEventListener("click", listener, { capture: true });
         //cleanup
         return () => {
-            document.removeEventListener('click', listener, { capture: true })
-        }
-    }, [])
+            document.removeEventListener("click", listener, { capture: true });
+        };
+    }, []);
 
     if (editing) {
         return (
-            <div ref={watcherRef}>
-                <MDEditor />
+            <div className="text-editor card" ref={watcherRef}>
+                <div className="card-content">
+                    <MDEditor
+                        value={markDownValue}
+                        onChange={(d) => setMarkDownValue(d || "")}
+                    />
+                </div>
             </div>
-        )
+        );
     }
     return (
         <div onClick={() => setEditing(true)}>
-            <MDEditor.Markdown source={"# Header"} />
+            <MDEditor.Markdown source={markDownValue} />
         </div>
-    )
-}
+    );
+};
 
-export default MarkDownEditor
+export default MarkDownEditor;
