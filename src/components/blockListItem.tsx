64e7ffca1a669bb9b9731acbf2c8c6e468a43e07
harshattray <harsh.attray@gmail.com>
2021-03-18 08:23:09 +0100

    Styling changes

--- src/components/blockListItem.tsx
+++ src/components/blockListItem.tsx
@@ -1,23 +1,35 @@
-import { Vault } from '../core';
-import CodeVaultComponent from './codevault';
-import MarkDownEditor from './MarkdownEditorComponent/markdowneditor'
+import { Vault } from "../core";
+import CodeVaultComponent from "./codevault";
+import MarkDownEditor from "./MarkdownEditorComponent/markdowneditor";
+import ControlCenterComponent from "./controlcenter";
+import "./blockListItem.css";
+import { Fragment } from "react";
 
 interface BlockListItemProps {
-    block: Vault
+    block: Vault;
 }
 
 const BlockListItem: React.FC<BlockListItemProps> = ({ block }) => {
     let child: JSX.Element;
 
-    if (block.type === 'code') {
-        child = <CodeVaultComponent block={block} />
+    if (block.type === "code") {
+        child = (
+            <Fragment>
+                <div className="control-center-wrapper">
+                    <ControlCenterComponent id={block.id} />
+                </div>
+                <CodeVaultComponent block={block} />
+            </Fragment>
+        );
+    } else {
+        child = (
+            <Fragment>
+                <MarkDownEditor block={block} />
+                <ControlCenterComponent id={block.id} />
+            </Fragment>
+        );
     }
-    else {
-        child = <MarkDownEditor block={block} />
-    }
-    return (
-        <div>{child}</div>
-    )
-}
+    return <div className="block-list-item">{child}</div>;
+};
 
-export default BlockListItem
+export default BlockListItem;
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { store } from './core'
 // import CodeVaultComponent from "./components/codevault";
 // import MarkDownEditor from './components/TextEditorComponent/texteditor'
 import "bulmaswatch/darkly/bulmaswatch.min.css";
+import '@fortawesome/fontawesome-free/css/all.min.css'
 import BlockList from "./components/blockList";
 
 
